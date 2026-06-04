9d5c976012fe84c738dab7207e8be7dfdc251928
knucklessg1 <knucklessg1@gmail.com>
2026-06-03 23:58:17 -0500

    Bump version: 0.25.0 → 0.26.0

--- qbittorrent_agent/mcp_server.py
+++ qbittorrent_agent/mcp_server.py
@@ -41,7 +41,7 @@ from starlette.responses import JSONResponse
 
 from qbittorrent_agent.auth import get_client
 
-__version__ = "0.25.0"
+__version__ = "0.26.0"
 
 logger = get_logger(name="qbittorrent-agent")
 logger.setLevel(logging.INFO)
